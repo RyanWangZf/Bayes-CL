a78667370ac589206f75f377858eb66d40fb7fcd
Zifeng Wang <1399320648@qq.com>
2020-07-10 14:24:30 +0800

    fix bugs in plot_out.ipynb

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-srun --gres=gpu:1 python -u main_nocl.py train --use_gpu=True > train.log &
+srun --gres=gpu:1 python -u main_nocl.py train --use_gpu=True --batch_size=128 > train_nocl.log &
+#srun --gres=gpu:1 python -u main_dclif.py train --use_gpu=True --batch_size=128 > train.log &
 
